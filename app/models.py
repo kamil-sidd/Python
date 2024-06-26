92ce629be7696633c486650690231a6b73535c47
kamil <kmlsddq53@gmail.com>
2024-06-26 19:04:10 +0530

    updating sub module

--- app/models.py
+++ app/models.py
@@ -1,12 +1,17 @@
 from django.db import models
 import datetime
+
+
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.name 
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
 
 
 class Customer(models.Model):
@@ -22,13 +27,16 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    price = models.DecimalField(default = 0, decimal_places=2, max_digits=6)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
-    description = models.CharField(max_length=250, default='', blank=True, null=True )
+    description = models.CharField(max_length=250, default='', blank=True, null=True)
     image = models.ImageField(upload_to="upload/product/")
+    is_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
 
     def __str__(self):
-        return self.name 
+        return self.name
+
 
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
@@ -38,6 +46,6 @@ class Order(models.Model):
     phone = models.CharField(max_length=20, default='', blank=True)
     date = models.DateField(default=datetime.datetime.today)
     status = models.BooleanField(default=False)
-    
+
     def __str__(self):
         return self.product
--- project/settings.py
+++ project/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = ['static/']
 
 import os
 MEDIA_URL = 'media/'
